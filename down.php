c5a21c19d4b37cde0a56fcd10396595bd6e4dceb
CUI-hyg <33583919@qq.com>
2024-10-31 20:47:16 +0800

    Update down.php

--- down.php
+++ down.php
@@ -1,9 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0; url=https://183-255-110-76.pd1.cjjd19.com:30443/download-cdn.cjjd19.com/123-660/8b223e79/1837594907-0/8b223e79717ed82efdca79772d7d7e94/c-m56?v=5&t=1730464807&s=1730464807d5ff26c4171e7d9e467e1d9ab99a85eb&r=ALCYOA&bzc=1&bzs=1837594907&filename=down.txt&x-mf-biz-cid=33409a82-4b26-4cd4-82b6-e8e4b110eea9-6eaa77&auto_redirect=0&cache_type=1&xmfcid=0e983fc2-3c18-4344-b167-f1320fe42a1d-0-50111d3b1">
-</head>
-</body>
-</html>
+[wechat]
+"https://116-142-255-142.pd1.cjjd19.com:30443/download-cdn.cjjd19.com/123-64/a64cfbd8/1837594907-0/a64cfbd8dcc8638a881b51ce7b762040/c-m37?v=5&t=1727526120&s=1727526120e1ae152351b2ceca3cde051a31207504&r=ED60TB&bzc=1&bzs=1837594907&filename=Wechat.7z&x-mf-biz-cid=34c27305-f2bf-447a-ad13-592bb9fb59cc-584000&auto_redirect=0&cache_type=1&xmfcid=2dad7dee-75ec-40d7-9dda-f2f03d8d5229-1-50111d3b1"
